e401a7aa699a5d1737127903bbc69ca7c179a7b1
PawelRa <pawelrat@poczta.onet.pl>
2023-03-21 16:49:08 +0100

    Determine offspring sex based on genes XX and XY chromosomes

---
+++ Determine offspring sex based on genes XX and XY chromosomes/ChromosomeCheck.cs
@@ -0,0 +1,14 @@
+﻿namespace Determine_offspring_sex_based_on_genes_XX_and_XY_chromosomes
+{
+    public class Chromosome
+    {
+        public static string ChromosomeCheck(string sperm)
+        {
+            if (sperm == "XY")
+            {
+                return "Congratulations! You're going to have a son.";
+            }
+            return "Congratulations! You're going to have a daughter.";
+        }
+    }
+}
---
+++ Determine offspring sex based on genes XX and XY chromosomes/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Determine_offspring_sex_based_on_genes_XX_and_XY_chromosomes;
+
+Console.WriteLine(Chromosome.ChromosomeCheck("XY"));
