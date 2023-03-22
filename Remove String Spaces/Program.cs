a358fa516dbbede4ad7134497b943e2463b44153
PawelRa <pawelrat@poczta.onet.pl>
2023-03-22 13:17:26 +0100

    Remove String Spaces

---
+++ Remove String Spaces/Program.cs
@@ -0,0 +1,3 @@
+﻿using Remove_String_Spaces;
+
+Console.WriteLine(SpacesRemover.NoSpace("8 j 8 1  mBliB8g  imjB8B8  jl  B"));
---
+++ Remove String Spaces/SpacesRemover.cs
@@ -0,0 +1,19 @@
+﻿namespace Remove_String_Spaces
+{
+    public static class SpacesRemover
+    {
+        public static string NoSpace(string input)
+        {
+            string Result = "";
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    continue;
+                }
+                Result += c;
+            }
+            return Result;
+        }
+    }
+}
