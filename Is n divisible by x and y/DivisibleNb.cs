53a882e1c6ad0ed51afe27594d67feddc04fe5cd
PawelRa <pawelrat@poczta.onet.pl>
2023-03-24 11:52:10 +0100

    Is n divisible by x and y?

---
+++ Is n divisible by x and y/DivisibleNb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_n_divisible_by_x_and_y
+{
+    public class DivisibleNb
+    {
+        public static bool IsDivisible(int n, int x, int y)
+        {
+            return (n % x == 0 && n % y == 0);
+        }
+    }
+}
---
+++ Is n divisible by x and y/Program.cs
@@ -0,0 +1,2 @@
+﻿
+Console.WriteLine(Is_n_divisible_by_x_and_y.DivisibleNb.IsDivisible(3, 3, 4));
