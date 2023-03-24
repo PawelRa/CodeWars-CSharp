570c7cb2add65013da7e1fb33c51804ca5e959d0
PawelRa <pawelrat@poczta.onet.pl>
2023-03-24 10:59:15 +0100

    Square(n) Sum

---
+++ Square(n) Sum/Kata.cs
@@ -0,0 +1,17 @@
+﻿namespace Square_n__Sum
+{
+    public static class Kata
+    {
+        public static int SquareSum(int[] numbers)
+        {
+            if (numbers == null) { return 0; }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number * number;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Square(n) Sum/Program.cs
@@ -0,0 +1,2 @@
+﻿
+Console.WriteLine(Square_n__Sum.Kata.SquareSum(new int[] { 1, 2, 2 }));
