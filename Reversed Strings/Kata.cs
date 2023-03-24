6a4e55464d02c4f30d33b96ceda897677904db52
PawelRa <pawelrat@poczta.onet.pl>
2023-03-24 14:30:56 +0100

    Reversed Strings one line result

--- Reversed Strings/Kata.cs
+++ Reversed Strings/Kata.cs
@@ -10,12 +10,7 @@ namespace Reversed_Strings
     {
         public static string Solution(string str)
         {
-            string result = "";
-            for (int i = str.Length - 1; i >= 0; i--)
-            {
-                result += str[i];
-            }
-            return result;
+            return new string(str.ToArray().Reverse().ToArray());
         }
     }
 }
