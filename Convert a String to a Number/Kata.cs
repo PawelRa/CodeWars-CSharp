7bf38d581ac0cb290e7f370de207199b865f7f06
PawelRa <pawelrat@poczta.onet.pl>
2023-03-29 09:28:18 +0200

    Convert a String to a Number!

---
+++ Convert a String to a Number/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Convert_a_String_to_a_Number
+{
+    public class Kata
+    {
+        public static int StringToNumber(String str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+}
---
+++ Convert a String to a Number/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Convert_a_String_to_a_Number.Kata.StringToNumber("1234"));
