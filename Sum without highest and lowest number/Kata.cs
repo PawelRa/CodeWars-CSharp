1f5bb8c32af84b11a770bb386a5549166bbeb42c
PawelRa <pawelrat@poczta.onet.pl>
2023-03-29 09:04:45 +0200

    Sum without highest and lowest number

---
+++ Sum without highest and lowest number/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_without_highest_and_lowest_number
+{
+    public static class Kata
+    {
+        public static int Sum(int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 2) return 0;
+
+            return numbers.Sum() - numbers.Max() - numbers.Min();
+        }
+    }
+}
---
+++ Sum without highest and lowest number/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Sum_without_highest_and_lowest_number.Kata.Sum(new[] { 6,3 }));
