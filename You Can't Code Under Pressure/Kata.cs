6b3ebea34a5028c8d4db2be08b267d28802517fe
PawelRa <pawelrat@poczta.onet.pl>
2023-04-03 21:45:08 +0200

    You Can't Code Under Pressure #1

---
+++ You Can't Code Under Pressure/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace You_Can_t_Code_Under_Pressure
+{
+    public static class Kata
+    {
+        public static int DoubleInteger(int n)
+        {
+            return n * 2;
+        }
+    }
+}
---
+++ You Can't Code Under Pressure/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(You_Can_t_Code_Under_Pressure.Kata.DoubleInteger(2));
