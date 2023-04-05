4604f0308355d89b85819434738f9c78ca4e5a6a
PawelRa <pawelrat@poczta.onet.pl>
2023-04-05 14:30:39 +0200

    Reverse List Order

---
+++ Reverse List Order/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Reverse_List_Order
+{
+    public class Kata
+    {
+        public static List<int> ReverseList(List<int> list)
+        {
+            List<int> result = new List<int>();
+            foreach (int el in list)
+            {
+                result.Insert(0, el);
+            }
+            return result;
+        }
+    }
+}
---
+++ Reverse List Order/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join("", Reverse_List_Order.Kata.ReverseList(new List<int> { 1, 2, 3, 4 })));
