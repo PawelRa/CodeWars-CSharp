95300c36e33ede6ffadc9287a2dcd846d40ed0bf
PawelRa <pawelrat@poczta.onet.pl>
2023-04-07 10:50:17 +0200

    The highest profit wins! - one line result

--- The highest profit wins/MinMax.cs
+++ The highest profit wins/MinMax.cs
@@ -1,21 +1,10 @@
-﻿using System;
-
-namespace The_highest_profit_wins
+﻿namespace The_highest_profit_wins
 {
     public class MinMax
     {
         public static int[] minMax(int[] lst)
         {
-            if (lst.Length == 1) { return new int[] { lst[0], lst[0] }; }
-            
-            int[] result = {int.MaxValue,int.MinValue};
-            foreach (int number in lst)
-            {
-                if (number < result[0]) { result[0] = number; }
-                if (number > result[1]) { result[1] = number; }
-            }
-
-            return result;
+            return new int[] { lst.Min(), lst.Max() };
         }
     }
 }
