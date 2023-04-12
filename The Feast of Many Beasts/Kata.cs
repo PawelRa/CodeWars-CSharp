9180e6c45dc2c20850df8f6b195cae6d4368c7a8
PawelRa <pawelrat@poczta.onet.pl>
2023-04-12 12:29:35 +0200

    The Feast of Many Beasts

---
+++ The Feast of Many Beasts/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Feast_of_Many_Beasts
+{
+    public class Kata
+    {
+        public static bool Feast(string beast, string dish)
+        {
+            return (beast[0] == dish[0]) && (beast[beast.Length-1] == dish[dish.Length-1]);
+        }
+    }
+}
