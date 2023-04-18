946b0658435692ebe698e338387ada3c8f1d5b6a
PawelRa <pawelrat@poczta.onet.pl>
2023-04-18 08:26:23 +0200

    shorter Find the capitals

--- Find the capitals/Kata.cs
+++ Find the capitals/Kata.cs
@@ -7,23 +7,16 @@ namespace Find_the_capitals
         public static int[] Capitals(string word)
         {
             List<int> tempList = new List<int>();
-            
 
-            for (int i = 0; i<word.Length; i++)
+            for (int i = 0; i < word.Length; i++)
             {
                 if (char.IsUpper(word[i]))
                 {
                     tempList.Add(i);
                 }
             }
-            int[] result = new int[tempList.Count];
 
-            for (int i = 0; i < tempList.Count; i++)
-            {
-                result[i] = tempList[i];
-            }
-
-            return result;
+            return tempList.ToArray();
         }
     }
 }
