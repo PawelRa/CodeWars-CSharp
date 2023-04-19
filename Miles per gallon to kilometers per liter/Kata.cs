e208051782873a1b53af47c4af222500199a04d9
PawelRa <pawelrat@poczta.onet.pl>
2023-04-19 08:00:31 +0200

    Miles per gallon to kilometers per liter

---
+++ Miles per gallon to kilometers per liter/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace Miles_per_gallon_to_kilometers_per_liter
+{
+    public static class Kata
+    {
+        public static double Converter(int mpg)
+        {
+            return Math.Round(mpg / (4.54609188 / 1.609344),2);
+        }
+    }
+}
