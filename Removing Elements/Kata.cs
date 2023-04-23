a58c5ced0c4e444e3bdd264dd924aedebe28f583
PawelRa <pawelrat@poczta.onet.pl>
2023-04-23 14:12:09 +0200

    porządki

---
+++ Removing Elements/Kata.cs
@@ -0,0 +1,26 @@
+﻿namespace Removing_Elements
+{
+    public static class Kata
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+            if (arr.Length < 3) { return arr; }
+
+            object[] result = new object[arr.Length];
+            int empty = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result[i] = arr[i];
+                }
+                else
+                {
+                    empty++;
+                }
+            }
+            Console.WriteLine(empty);
+            return result;
+        }
+    }
+}
---
+++ Removing Elements/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine(string.Join(" ", Removing_Elements.Kata.RemoveEveryOther(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })));
+
+
+//https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/csharp
