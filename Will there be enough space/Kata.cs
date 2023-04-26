422ec46695677eb291e366542c0219f24ad9fa97
PawelRa <pawelrat@poczta.onet.pl>
2023-04-26 13:28:02 +0200

    Will there be enough space?

---
+++ Will there be enough space/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Will_there_be_enough_space
+{
+    public static class Kata
+    {
+        public static int Enough(int cap, int on, int wait)
+        {
+            return (cap - on - wait) >= 0 ? 0 : on + wait - cap;
+        }
+    }
+}
---
+++ Will there be enough space/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Will_there_be_enough_space.Kata.Enough(100, 60, 50));
