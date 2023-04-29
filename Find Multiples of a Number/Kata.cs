680f2cd410923c1d9b1eb933f3ca276f932ea3f6
PawelRa <pawelrat@poczta.onet.pl>
2023-04-29 08:18:16 +0200

    Find Multiples of a Number

---
+++ Find Multiples of a Number/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Multiples_of_a_Number
+{
+
+    public class Kata
+    {
+        public static List<int> FindMultiples(int integer, int limit)
+        {
+           List<int> result = new List<int>();
+           for (int i = 1; integer*i<=limit; i++)
+            {
+                result.Add(integer*i);
+                Console.WriteLine(integer * i);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Find Multiples of a Number/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Find_Multiples_of_a_Number.Kata.FindMultiples(5,25));
