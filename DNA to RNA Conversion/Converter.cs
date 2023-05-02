c6de96a5f95459cfdf486df24548aeb20d3924de
PawelRa <pawelrat@poczta.onet.pl>
2023-05-02 11:21:14 +0200

    DNA to RNA Conversion

---
+++ DNA to RNA Conversion/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNAtoRNAConversion
+{
+    public class Converter
+    {
+        public static string dnaToRna(string dna)
+        {
+            string result = "";
+
+            foreach (char letter  in dna)
+            {
+                if (letter == 'T')
+                {
+                    result += "U";
+                }  else
+                {
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DNA to RNA Conversion/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(DNAtoRNAConversion.Converter.dnaToRna("TTTT"));
