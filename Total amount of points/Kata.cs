4ec5b6e4a35f939e0d29408968360e30d0f88c5f
PawelRa <pawelrat@poczta.onet.pl>
2023-05-08 08:12:42 +0200

    Total amount of points

---
+++ Total amount of points/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total_amount_of_points
+{
+    public static class Kata
+    {
+        public static int TotalPoints(string[] games)
+        {
+            int score = 0;
+
+            for (int i = 0; i < games.Length; i++)
+            {
+                if (games[i][0] > games[i][2])
+                {
+                    score += 3;
+                }
+                else if (games[i][0] == games[i][2])
+                {
+                    score += 1;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ Total amount of points/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Total_amount_of_points.Kata.TotalPoints(new[] { "1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4" }));
