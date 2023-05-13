664019b930bfce68a2eea501a5227e7aaa26d9fc
PawelRa <pawelrat@poczta.onet.pl>
2023-05-13 13:59:26 +0200

    Find the first non-consecutive number

---
+++ Find the first non-consecutive number/Kata.cs
@@ -0,0 +1,18 @@
+﻿namespace Find_the_first_non_consecutive_number
+{
+    public class Kata
+    {
+        public static object FirstNonConsecutive(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] != 1)
+                {
+                    return arr[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Find the first non-consecutive number/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Find_the_first_non_consecutive_number.Kata.FirstNonConsecutive(new int[] { 1, 2, 3, 4, 6, 7, 8 }));
