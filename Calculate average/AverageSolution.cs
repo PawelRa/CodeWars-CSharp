6e672e05c45d7599e2f8369b17712a38e9391c66
PawelRa <pawelrat@poczta.onet.pl>
2023-05-14 07:53:10 +0200

    Calculate average

---
+++ Calculate average/AverageSolution.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculate_average
+{
+    class AverageSolution
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length < 1) return 0;
+            double arraySum = 0;
+            foreach (var number in array)
+            {
+                arraySum += number;
+            }
+            return arraySum / (array.Length);
+        }
+    }
+}
---
+++ Calculate average/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Calculate_average.AverageSolution.FindAverage(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 }));
