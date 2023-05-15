13b1e070cc7ef8ac97f718e1a9504897dd58e80a
PawelRa <pawelrat@poczta.onet.pl>
2023-05-15 08:42:12 +0200

    What is between?

---
+++ What is between/Kata.cs
@@ -0,0 +1,16 @@
+﻿namespace What_is_between
+{
+    public class Kata
+    {
+        public static int[] Between(int a, int b)
+        {
+            int[] result = new int[b - a + 1];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = a + i;
+            }
+            return result;
+        }
+    }
+}
---
+++ What is between/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine($"{string.Join(", ", What_is_between.Kata.Between(-2, 2))}");
