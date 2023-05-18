bd31a5d825b59989cd4ee48c3241b53a119a2c5f
PawelRa <pawelrat@poczta.onet.pl>
2023-05-18 09:17:42 +0200

    Multiple of index

---
+++ Multiple of index/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_of_index
+{
+    public static class Kata
+    {
+        public static List<int> MultipleOfIndex(List<int> xs)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 1; i < xs.Count; i++)
+            {
+                if (xs[i] % i == 0)
+                {
+                    result.Add(xs[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Multiple of index/Program.cs
@@ -0,0 +1,2 @@
+﻿
+Console.WriteLine(string.Join(", ", Multiple_of_index.Kata.MultipleOfIndex(new List<int> { 22, -6, 32, 82, 9, 25 })));
