5bd4213052f36bf7d6b374c6cc804bb4f9fabb28
PawelRa <pawelrat@poczta.onet.pl>
2023-05-27 08:40:54 +0200

    Jenny's secret message

---
+++ Jenny's secret message/Kata.cs
@@ -0,0 +1,13 @@
+﻿namespace Jenny_s_secret_message
+{
+    public static class Kata
+    {
+        public static string greet(string name)
+        {
+            if (name == "Johnny")
+                return "Hello, my love!";
+
+            return "Hello, " + name + "!";
+        }
+    }
+}
