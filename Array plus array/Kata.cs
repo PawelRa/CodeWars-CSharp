5506492d3d2ddaa0815481a7946ecdfd4e2616db
PawelRa <pawelrat@poczta.onet.pl>
2023-05-28 10:11:17 +0200

    Array plus array

---
+++ Array plus array/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_plus_array
+{
+    public static class Kata
+    {
+        public static int ArrayPlusArray(int[] arr1, int[] arr2)
+        {
+            int result = 0;
+
+            foreach (int i in arr1) { result += i; }
+
+            foreach (int i in arr2) { result += i; }
+
+            return result;
+        }
+    }
+}
---
+++ Array plus array/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Array_plus_array.Kata.ArrayPlusArray(new int[] { 100, 200, 300 }, new int[] { 400, 500, 600 }));
