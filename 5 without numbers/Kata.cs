4225fba3230195ecfd5a7c67ad4ccfea74114bd4
PawelRa <pawelrat@poczta.onet.pl>
2023-06-01 08:30:14 +0200

    5 without numbers !!

---
+++ 5 without numbers/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_without_numbers
+{
+    class Kata
+    {
+        public static int UnusualFive()
+        {
+            string[] result = new string[] {"a","b","c","d","e","f"};
+            return Array.IndexOf(result,"f");
+        }
+    }
+}
---
+++ 5 without numbers/Program.cs
@@ -0,0 +1,3 @@
+﻿// Write a function that always returns 5
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+Console.WriteLine(_5_without_numbers.Kata.UnusualFive());
