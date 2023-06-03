0f206558b96a88a3785b8b72ff5d415b06df727e
PawelRa <pawelrat@poczta.onet.pl>
2023-06-03 11:22:39 +0200

    Returning Strings

---
+++ Returning Strings/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace Returning_Strings
+{
+    public static class Kata
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello, {name} how are you doing today?";
+        }
+    }
+}
---
+++ Returning Strings/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Returning_Strings.Kata.Greet("Ryan"));
