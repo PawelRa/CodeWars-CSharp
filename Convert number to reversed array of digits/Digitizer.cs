96e7555957c810d88c15962578a909038613f027
PawelRa <pawelrat@poczta.onet.pl>
2023-06-06 11:33:57 +0200

    Convert number to reversed array of digits

---
+++ Convert number to reversed array of digits/Digitizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Convert_number_to_reversed_array_of_digits
+{
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            string nString = n.ToString();
+
+            long[] result = new long[nString.Length];
+            for (int i = nString.Length - 1; i >= 0; i--)
+            {
+                result[nString.Length - i - 1] = (long)Char.GetNumericValue(nString[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Convert number to reversed array of digits/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(",", Convert_number_to_reversed_array_of_digits.Digitizer.Digitize(35231)));
