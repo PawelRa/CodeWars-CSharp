07d2bfd8990e16204e69c9c356781762ddf0b223
PawelRa <pawelrat@poczta.onet.pl>
2023-06-10 08:14:33 +0200

    Find the smallest integer in the array

---
+++ Find the smallest integer in the array/Kata.cs
@@ -0,0 +1,15 @@
+﻿namespace Find_the_smallest_integer_in_the_array
+{
+    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            int min = int.MaxValue;
+            foreach (int value in args)
+            {
+                if (value < min) min = value;
+            }
+            return min;
+        }
+    }
+}
---
+++ Find the smallest integer in the array/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Find_the_smallest_integer_in_the_array.Kata.FindSmallestInt(new int[] { 78, 56, 232, 12, 11, 43 }));
