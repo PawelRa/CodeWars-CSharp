f9c398ddd710e2b76e485f7a82672c92f45cd978
PawelRa <pawelrat@poczta.onet.pl>
2023-06-22 09:11:13 +0200

    Is this a triangle?

---
+++ Is this a triangle/Triangle.cs
@@ -0,0 +1,22 @@
+﻿namespace Is_this_a_triangle
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+            {
+                if (b + c >= a) return true;
+            }
+            else if (b >= a && b >= c)
+            {
+                if (c + a >= b) return true;
+            }
+            else if (c >= a && c >= b)
+            {
+                if (b + a >= c) { return true; }
+            }
+            return false;
+        }
+    }
+}
