20faa0ce139768476d7b830acf36d1266b1a44b5
PawelRa <pawelrat@poczta.onet.pl>
2023-06-22 09:12:39 +0200

    Is this a triangle? in one line

--- Is this a triangle/Program.cs
+++ Is this a triangle/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine(Is_this_a_triangle.Triangle.IsTriangle(2,2,2));
+﻿Console.WriteLine(Is_this_a_triangle.Triangle.IsTriangle(2, 2, 2));
--- Is this a triangle/Triangle.cs
+++ Is this a triangle/Triangle.cs
@@ -4,19 +4,7 @@
     {
         public static bool IsTriangle(int a, int b, int c)
         {
-            if (a >= b && a >= c)
-            {
-                if (b + c >= a) return true;
-            }
-            else if (b >= a && b >= c)
-            {
-                if (c + a >= b) return true;
-            }
-            else if (c >= a && c >= b)
-            {
-                if (b + a >= c) { return true; }
-            }
-            return false;
+            return (a + b > c && b + c > a && a + c > b);
         }
     }
 }
