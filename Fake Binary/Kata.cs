33fb9c9a7c14c7b7a2460f9c211b8f58addec7ed
PawelRa <pawelrat@poczta.onet.pl>
2023-06-25 11:40:53 +0200

    Fake Binary

---
+++ Fake Binary/Kata.cs
@@ -0,0 +1,23 @@
+﻿namespace Fake_Binary
+{
+    public class Kata
+    {
+        public static string FakeBin(string x)
+        {
+            string result = "";
+
+            foreach (char number in x)
+            {
+                if (int.Parse(number.ToString()) < 5)
+                {
+                    result += 0;
+                }
+                else if (int.Parse(number.ToString()) >= 5)
+                {
+                    result += 1;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Fake Binary/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Fake_Binary.Kata.FakeBin("45385593107843568"));
