9028cf16856932d31065afb128c55d5cfcdbb5fe
PawelRa <pawelrat@poczta.onet.pl>
2023-07-11 10:34:00 +0200

    Sentence Smash - one line result

--- Sentence Smash/Kata.cs
+++ Sentence Smash/Kata.cs
@@ -1,26 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sentence_Smash
+﻿namespace Sentence_Smash
 {
     public class Kata
     {
         public static string Smash(string[] words)
         {
-            if (words.Length < 1) { return ""; }
-            if (words.Length == 1) { return words[0]; }
-
-            string result = "";
-            for (int i = 0; i < words.Length - 1; i++)
-            {
-                result += words[i] + " ";
-            }
-            result += words[words.Length - 1];
-
-            return result;
+            return string.Join(" ", words);
         }
     }
 }
