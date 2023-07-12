ca9455cce8a6d1098e22a04d2f59232994e0beb1
PawelRa <pawelrat@poczta.onet.pl>
2023-07-12 11:28:43 +0200

    A Needle in the Haystack

---
+++ A Needle in the Haystack/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A_Needle_in_the_Haystack
+{
+    public class Kata
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] is string && haystack[i] == "needle")
+                {
+                    return "found the needle at position " + i;
+                }
+            }
+
+            return "bad";
+        }
+    }
+}
---
+++ A Needle in the Haystack/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(A_Needle_in_the_Haystack.Kata.FindNeedle(new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, "needle", 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54 }));
