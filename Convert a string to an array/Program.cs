719424d2e212f327ef60a9b50c8b0903d814dc55
PawelRa <pawelrat@poczta.onet.pl>
2023-07-14 09:55:21 +0200

    Convert a string to an array

---
+++ Convert a string to an array/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Convert_a_string_to_an_array.Solution.StringToArray("Robin Singh"));
---
+++ Convert a string to an array/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Convert_a_string_to_an_array
+{
+    public class Solution
+    {
+        public static string[] StringToArray(string str)
+        {
+            return str.Split(" ");
+        }
+    }
+}
