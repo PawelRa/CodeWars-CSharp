87368ee5aadb6e254cb4f7b5645e5e69d73aabb9
PawelRa <pawelrat@poczta.onet.pl>
2023-07-17 07:54:39 +0200

    Triple Trouble

--- Triple Trouble/Kata.cs
+++ Triple Trouble/Kata.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Triple_Trouble
+﻿namespace Triple_Trouble
 {
     public class Kata
     {
