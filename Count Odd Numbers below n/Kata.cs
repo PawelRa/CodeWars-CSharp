f0350fec5dd73d468c8ed00b2b526a1e373e8d97
PawelRa <p.ratajski@gmail.com>
2023-07-19 21:54:40 +0200

    Count Odd Numbers below n

---
+++ Count Odd Numbers below n/Kata.cs
@@ -0,0 +1,10 @@
+﻿namespace Count_Odd_Numbers_below_n
+{
+    public static class Kata
+    {
+        public static ulong OddCount(ulong n)
+        {
+            return n / 2;
+        }
+    }
+}
---
+++ Count Odd Numbers below n/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Count_Odd_Numbers_below_n.Kata.OddCount(10));
