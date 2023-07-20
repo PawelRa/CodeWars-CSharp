6f1096cd5c57b550e8a8fcb7545b957880bad3eb
PawelRa <pawelrat@poczta.onet.pl>
2023-07-20 13:51:07 +0200

    Remove First and Last Character Part Two

---
+++ Remove First and Last Character Part Two/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Remove_First_and_Last_Character_Part_Two
+{
+    public static class Kata
+    {
+        public static string Array(string s)
+        {
+            if (s.Length < 3) { return null; }
+            char[] separators = new char[] { ',', ' ' };
+            string[] array = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            if (array.Length < 2) { return null; }
+
+            string result = "";
+
+
+            for (int i = 1; i < array.Length - 2; i++)
+            {
+
+                result += array[i];
+                result += " ";
+            }
+            result += array[array.Length - 2];
+
+            return result;
+        }
+    }
+}
---
+++ Remove First and Last Character Part Two/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Remove_First_and_Last_Character_Part_Two.Kata.Array("1,2,3"));
