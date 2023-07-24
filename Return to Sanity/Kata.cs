0147181b7adc93971e85f0a66b31f5b32b846e8a
PawelRa <pawelrat@poczta.onet.pl>
2023-07-24 09:39:15 +0200

    Return to Sanity

---
+++ Return to Sanity/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Return_to_Sanity
+{
+    public class Kata
+    {
+        public static Dictionary<string, string> Mystery()
+        {
+            Dictionary<string,
+          string> results = new
+            Dictionary<string,
+          string>();
+            results.Add("sanity", "hello");
+            return results;
+        }
+    }
+}
---
+++ Return to Sanity/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(new Dictionary<string, string> { { "sanity", "hello" } });
