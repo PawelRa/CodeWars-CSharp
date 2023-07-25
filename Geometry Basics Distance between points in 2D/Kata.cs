624fb7ce7a48dc4f0af958de61e1fba60784e137
PawelRa <pawelrat@poczta.onet.pl>
2023-07-25 09:57:57 +0200

    Geometry Basics: Distance between points in 2D

---
+++ Geometry Basics Distance between points in 2D/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry_Basics_Distance_between_points_in_2D
+{
+    public class Kata
+    {
+        public static double DistanceBetweenPoints(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+    }
+}
