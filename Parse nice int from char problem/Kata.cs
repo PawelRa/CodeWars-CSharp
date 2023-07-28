a3b2b0022dd2d27d2d714ba4b03f05752389c1a4
PawelRa <pawelrat@poczta.onet.pl>
2023-07-28 13:04:13 +0200

    Parse nice int from char problem

---
+++ Parse nice int from char problem/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parse_nice_int_from_char_problem
+{
+    public class Kata
+    {
+        public static int GetAge(string inputString)
+        {
+            return Convert.ToInt32(new string(inputString[0], 1));
+        }
+    }
+}
---
+++ Parse nice int from char problem/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Parse_nice_int_from_char_problem.Kata.GetAge("9 years old"));
