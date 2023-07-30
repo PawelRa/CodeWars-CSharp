63ff7850ebcb7cd4b9704efaee7353f2174f059c
PawelRa <pawelrat@poczta.onet.pl>
2023-07-30 15:00:39 +0200

    Find Maximum and Minimum Values of a List

---
+++ Find Maximum and Minimum Values of a List/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Find_Maximum_and_Minimum_Values_of_a_List
+{
+    public class Kata
+    {
+        public int Min(int[] list)
+        {
+            return list.Min();
+        }
+
+        public int Max(int[] list)
+        {
+            return list.Max();
+        }
+    }
+}
