18457b6ed949f11d41b0d2b2a9a610eebaba4c39
PawelRa <pawelrat@poczta.onet.pl>
2023-08-01 17:37:04 +0200

    I love you, a little , a lot, passionately ... not at all

---
+++ I love you a little  a lot passionately  not at all/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace I_love_you_a_little__a_lot_passionately__not_at_all
+{
+    public class Kata
+    {
+        public static string HowMuchILoveYou(int nb_petals)
+        {
+            switch (nb_petals % 6)
+            {
+                case 1:
+                    return "I love you";
+                case 2:
+                    return "a little";
+                case 3:
+                    return "a lot";
+                case 4:
+                    return "passionately";
+                case 5:
+                    return "madly";
+                case 0:
+                    return "not at all";
+                default:
+                    return "-1";
+            }
+        }
+    }
+}
---
+++ I love you a little  a lot passionately  not at all/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(I_love_you_a_little__a_lot_passionately__not_at_all.Kata.HowMuchILoveYou(3));
