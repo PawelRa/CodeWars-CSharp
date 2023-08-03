7cea6ce4e108f64a90f5c23b5c31e935b9a2a36c
PawelRa <pawelrat@poczta.onet.pl>
2023-08-03 18:04:00 +0200

    Beginner - Lost Without a Map

---
+++ Beginner - Lost Without a Map/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginner___Lost_Without_a_Map
+{
+    public class Kata
+    {
+        public static int[] Maps(int[] x)
+        {
+            int[] result = new int[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                result[i] = x[i] * 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Beginner - Lost Without a Map/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Beginner___Lost_Without_a_Map.Kata.Maps(new[] { 1, 2, 3 }));
