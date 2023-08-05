efbbfa9e7633d94dcf81e02adf81dd249aa1fdec
PawelRa <pawelrat@poczta.onet.pl>
2023-08-05 11:12:44 +0200

    Remove exclamation marks

---
+++ Remove exclamation marks/Kata.cs
@@ -0,0 +1,24 @@
+﻿namespace Remove_exclamation_marks
+{
+    public class Kata
+    {
+        public static string RemoveExclamationMarks(string s)
+        {
+            if (s.Length < 1) { return ""; }
+            string result = "";
+
+            foreach (char c in s)
+            {
+                if (c == '!')
+                {
+                    continue;
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Remove exclamation marks/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Remove_exclamation_marks.Kata.RemoveExclamationMarks("Hi!"));
