dae20f080550a4e3b8002b78c736caeece145b82
PawelRa <pawelrat@poczta.onet.pl>
2023-08-08 13:40:58 +0200

    You only need one - Beginner

---
+++ You only need one - Beginner/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace You_only_need_one___Beginner
+{
+    public class Kata
+    {
+        public static bool Check(object[] a, object x)
+        {
+
+            return a.Contains(x);
+
+        }
+    }
+}
---
+++ You only need one - Beginner/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(You_only_need_one___Beginner.Kata.Check(new object[] { 66, 101 }, 66));
