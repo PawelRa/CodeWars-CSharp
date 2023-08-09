d61c86baa03bd04c618f767a4d4a8b242ee9de50
PawelRa <pawelrat@poczta.onet.pl>
2023-08-09 14:14:23 +0200

    Count of positives / sum of negatives

---
+++ Count of positives sum of negatives/Kata.cs
@@ -0,0 +1,19 @@
+﻿namespace Count_of_positives_sum_of_negatives
+{
+    public class Kata
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            int[] result = new int[] { 0, 0 };
+            if (input == null || input.Length < 1) { return new int[0]; }
+
+            foreach (int i in input)
+            {
+                if (i > 0) { result[0]++; }
+                if (i < 0) { result[1] += i; }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Count of positives sum of negatives/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(", ", Count_of_positives_sum_of_negatives.Kata.CountPositivesSumNegatives(new[] { 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14 })));
