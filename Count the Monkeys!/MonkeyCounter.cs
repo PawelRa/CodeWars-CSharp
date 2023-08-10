e4fe87b402479358ef4e9619b1bc659f6ba4becc
PawelRa <pawelrat@poczta.onet.pl>
2023-08-10 14:27:41 +0200

    Count the Monkeys!

---
+++ Count the Monkeys!/MonkeyCounter.cs
@@ -0,0 +1,16 @@
+﻿namespace Count_the_Monkeys_
+{
+    public static class MonkeyCounter
+    {
+        public static int[] MonkeyCount(int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0;i<n;i++)
+            {
+                result[i] = i+1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Count the Monkeys!/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(", ", Count_the_Monkeys_.MonkeyCounter.MonkeyCount(7)));
