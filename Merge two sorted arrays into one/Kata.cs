69ff1c5a7dec61ef44ed37b3089800cbc1269a15
PawelRa <pawelrat@poczta.onet.pl>
2023-08-14 21:06:39 +0200

    Merge two sorted arrays into one

---
+++ Merge two sorted arrays into one/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Merge_two_sorted_arrays_into_one
+{
+    public class Kata
+    {
+        public static int[] MergeArrays(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length < 1 && arr2.Length < 1) { return new int[] { }; }
+
+            int[] result = arr1.Concat(arr2).ToArray();
+            Array.Sort(result);
+
+            return result.Distinct().ToArray();
+        }
+    }
+}
---
+++ Merge two sorted arrays into one/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(", ", Merge_two_sorted_arrays_into_one.Kata.MergeArrays(new[] { 2, -3, -2, -1, 0 }, new int[] { 1, 2, 3, 4 })));
