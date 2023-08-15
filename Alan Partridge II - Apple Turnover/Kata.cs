d2fa40eea4708c12a03405b58c5c4ff9e5c6ee88
PawelRa <pawelrat@poczta.onet.pl>
2023-08-15 07:59:59 +0200

    Alan Partridge II - Apple Turnover

---
+++ Alan Partridge II - Apple Turnover/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Alan_Partridge_II___Apple_Turnover
+{
+    public class Kata
+    {
+        public static string Apple(object n)
+        {
+            int temperature = 0;
+
+            if (n.GetType() == typeof(string))
+            {
+
+                temperature = Convert.ToInt32(n.ToString());
+            }
+            else if (n.GetType() == typeof(int))
+            {
+                temperature = (int)n;
+            }
+
+            return temperature * temperature > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox.";
+        }
+    }
+}
---
+++ Alan Partridge II - Apple Turnover/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Alan_Partridge_II___Apple_Turnover.Kata.Apple("50"));
+
+Console.WriteLine(Alan_Partridge_II___Apple_Turnover.Kata.Apple(4));
