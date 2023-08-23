6108cc0266dd64b4d8010911e27bdf92f291e57e
PawelRa <pawelrat@poczta.onet.pl>
2023-08-23 12:48:23 +0200

    Sum Arrays

---
+++ Sum Arrays/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Sum_Arrays
+{
+    public class Kata
+    {
+        public static double SumArray(double[] array)
+        {
+            return array.Sum();
+        }
+    }
+}
---
+++ Sum Arrays/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Sum_Arrays.Kata.SumArray(new double[] { 1, 5.2, 4, 0, -1 }));
