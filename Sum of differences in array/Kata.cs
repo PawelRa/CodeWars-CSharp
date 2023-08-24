f1e2d1db068ec7da88acd473856a18eb3365eaca
PawelRa <pawelrat@poczta.onet.pl>
2023-08-24 13:04:37 +0200

    Sum of differences in array

---
+++ Sum of differences in array/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Sum_of_differences_in_array
+{
+    public static class Kata
+    {
+        public static int SumOfDifferences(int[] arr)
+        {
+            if (arr == null || arr.Length <=1) return 0;
+            arr = arr.OrderByDescending(x => x).ToArray();
+            int temp = 0;
+
+            for (int i = 1;i < arr.Length;i++)
+            {
+                temp += arr[i - 1] - arr[i];
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ Sum of differences in array/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Sum_of_differences_in_array.Kata.SumOfDifferences(new int[] { 0, 29, 24, 17 }));
+Console.WriteLine(Sum_of_differences_in_array.Kata.SumOfDifferences(new int[] { }));
+Console.WriteLine(Sum_of_differences_in_array.Kata.SumOfDifferences(new int[] { 17 }));
