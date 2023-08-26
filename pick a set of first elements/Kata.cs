e266fe6086edac40a9535c52d38602b2c3f4623f
PawelRa <pawelrat@poczta.onet.pl>
2023-08-26 13:47:50 +0200

    pick a set of first elements

---
+++ pick a set of first elements/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace pick_a_set_of_first_elements
+{
+    public class Kata
+    {
+        public static object[] TakeFirstElements(object[] array, int n = 1)
+        {
+            return array.Skip(0).Take(n).ToArray();
+        }
+    }
+}
---
+++ pick a set of first elements/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(
+    string.Join(", ", pick_a_set_of_first_elements.Kata.TakeFirstElements(new object[] { 'a', 'b', 'c', 'd', 'e' }, 3))
+    );
