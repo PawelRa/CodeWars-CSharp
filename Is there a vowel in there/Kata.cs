106fba6992fb0490cd978c2c480636c8f224cc61
PawelRa <pawelrat@poczta.onet.pl>
2023-09-29 16:24:44 +0200

    Is there a vowel in there?

---
+++ Is there a vowel in there/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_there_a_vowel_in_there
+{
+    public class Kata
+    {
+        public static object[] IsVow(object[] a)
+        {
+            object[] result = new object[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Convert.ToInt32(a[i]) == 117) { result[i] = "u"; }
+                else if (Convert.ToInt32(a[i]) == 111) { result[i] = "o"; }
+                else if (Convert.ToInt32(a[i]) == 105) { result[i] = "i"; }
+                else if (Convert.ToInt32(a[i]) == 101) { result[i] = "e"; }
+                else if (Convert.ToInt32(a[i]) == 97) { result[i] = "a"; }
+                else { result[i] = a[i]; }
+            }
+            return result;
+        }
+    }
+}
---
+++ Is there a vowel in there/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Is_there_a_vowel_in_there.Kata.IsVow(new object[] { 101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103 }));
